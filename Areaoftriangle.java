733598f7441f642f524e1bdfea9ca3254c86d80c
shubham01kumar <shubham365342@gmail.com>
2025-04-01 18:11:27 +0530

    Add files via upload

---
+++ Areaoftriangle.java
@@ -0,0 +1,20 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Areaoftriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the Base of Triangle : ");
+        int b=sc.nextInt();
+
+        System.out.print("Enter the Height of Triangle : ");
+        int h=sc.nextInt();
+
+        // 
+        double Area = (b*h)/2;
+
+        System.out.println("The Area of Triangle is : " + Area + "Cm2");
+    }
+}
